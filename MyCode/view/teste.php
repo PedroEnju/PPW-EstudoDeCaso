96cb7804d94fa97b209707b1c149774ab3b68ac8
PedroEnju <pedro_enju@hotmail.com>
2019-11-22 20:58:32 -0300

    22/11/1999 - Comments

--- MyCode/view/teste.php
+++ MyCode/view/teste.php
@@ -5,10 +5,12 @@ include_once __DIR__ . "/../library/Xml.php";
 include_once __DIR__ . "/../functions/state/estado.php";
 $estados = getAll();
 
+// EXPORTAR
 // $xml = new Xml('estado', $estados, array('nome_estado', 'uf'));
 // $manipulator = new XMLManipulator($xml);
 // $manipulator->export();
 
+// IMPORTAR
 $xml2 = new Xml('estado', null, array('nome_estado', 'uf'));
 $manipulator = new XMLManipulator($xml2);
 $manipulator->import();
